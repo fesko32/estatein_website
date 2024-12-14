3618be0b5a92112b91deb1b5d1b0d770f072db97
Ilya <fesko_il@icloud.com>
2024-12-14 09:20:06 +0300

    adds scripts for header

--- src/js/main.js
+++ src/js/main.js
@@ -3,22 +3,23 @@ import '../scss/styles.scss';
 const buttonClose = document.querySelector('.header__top-close-button');
 const banner = document.querySelector('.header__top');
 const burgerButton = document.querySelector('.header__bottom-burger-menu');
-const headeList = document.querySelector('.header__list');
+const headerList = document.querySelector('.header__list');
 
 // Проверяем, найден ли элемент
 
   // Функция для скрытия баннера
   function closeMenu() {
-    banner.classList.add('hidden'); // Добавляем класс "hidden"
+    banner.classList.add('hidden');
+    
   }
   
 
   // Привязываем событие клика к кнопке
   buttonClose.addEventListener('click', closeMenu);
+  
 
-
-  function openMenu() {
-    headeList.classList.add('open')
+  function toggleMenu() {
+    headerList.classList.toggle('open');
   }
-
-  burgerButton.addEventListener('click', openMenu);
+  
+  burgerButton.addEventListener('click', toggleMenu);
