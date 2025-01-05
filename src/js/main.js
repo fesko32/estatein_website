731117d6eb234bbc8212656d9ac01815b54e896c
fesko32 <fesko_il@icloud.com>
2025-01-05 19:53:55 +0300

    adds edits to the font

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
+import '../scss/global/global.scss';
 import '../scss/styles.scss';
 
 const buttonClose = document.querySelector('.header__top-close-button');
 const banner = document.querySelector('.header__top');
 const burgerButton = document.querySelector('.header__bottom-burger-menu');
 const headerList = document.querySelector('.header__list');
+const heroItems = document.querySelectorAll('.hero__item');
 
 // Проверяем, найден ли элемент
 
